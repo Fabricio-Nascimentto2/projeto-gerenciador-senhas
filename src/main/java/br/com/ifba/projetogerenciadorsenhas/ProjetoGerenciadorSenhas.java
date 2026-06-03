dbfb0b546fd0efd6ca27638f32d2c83ad899883b
Fabricio-Nascimentto2 <fabriciocostanascimento3@gmail.com>
2026-06-03 20:08:15 -0300

    feat: estrutura inicial das telas do gerenciador de senhas

---
+++ src/main/java/br/com/ifba/projetogerenciadorsenhas/ProjetoGerenciadorSenhas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.projetogerenciadorsenhas;
+
+/**
+ *
+ * @author fabricio
+ */
+public class ProjetoGerenciadorSenhas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
